2093a54066777038089aabf59baf4b1d5644c64c
Christine Contreras <christineec23@yahoo.com>
2021-05-06 13:10:39 -0500

    finished challenge #4

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ console.log('%c HI', 'color: firebrick');
 
 const init = () => {
     //challenge #1
-    const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+    // const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
 
-    fetch(imgUrl)
-    .then(response => response.json())
-    .then(json => {
+    // fetch(imgUrl)
+    // .then(response => response.json())
+    // .then(json => {
 
-        //adds image elements to the DOM for each image in the array
-        const imgContainer = document.getElementById('dog-image-container');
+    //     //adds image elements to the DOM for each image in the array
+    //     const imgContainer = document.getElementById('dog-image-container');
 
-        // console.log(json);
-        json.message.forEach(element => {
-            const imgTag = document.createElement('img');
-            imgTag.src = element; 
-            imgContainer.appendChild(imgTag);
-        });
-    }); //challenge #1
+    //     // console.log(json);
+    //     json.message.forEach(element => {
+    //         const imgTag = document.createElement('img');
+    //         imgTag.src = element; 
+    //         imgContainer.appendChild(imgTag);
+    //     });
+    // }); //challenge #1
 
 
     //challenge #2
@@ -64,10 +64,41 @@ const init = () => {
     //challenge #3
     //Once all of the breeds are rendered in the <ul>, add JavaScript so that, when the user clicks on any one of the <li>s, the font color of that <li> changes. This can be a color of your choosing.
     list.addEventListener('click', (e) => {
-        console.log(e.target);
+        // console.log(e.target);
 
         e.target.style.color = "green";
     })
+
+
+    //challenge #4 
+    //Once we are able to load all of the dog breeds onto the page, add JavaScript so that the user can filter breeds that start with a particular letter using a dropdown (Links to an external site.).
+    const breedDropdown = document.getElementById('breed-dropdown');
+
+    breedDropdown.addEventListener('change', (e) => {
+        //console.log(e.target.value);
+        const optionValue = e.target.value;
+
+        //turn li node list into array
+        const listArray = Array.from(document.querySelectorAll('#dog-breeds > li'));
+
+        // console.log(listArray)
+
+        //find innertext of each li element and display accrodingly
+        listArray.forEach(element => {
+            // console.log(element);
+            let liText = element.innerText;
+
+            if (liText.startsWith(optionValue)){
+                element.style.display = "block";
+            } else {
+                element.style.display = "none";
+            }
+
+        })
+
+
+    });
+
 
 
 
